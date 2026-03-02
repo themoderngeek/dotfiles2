925a8286fcde885ff1e78c66af1a9c3ad6224d54
Mark Taylor <marktaycon@gmail.com>
2026-03-02 21:27:51 +0000

    Made the battery script show percentage even if power is connected

--- tmux/scripts/battery.sh
+++ tmux/scripts/battery.sh
@@ -2,15 +2,19 @@
 
 # Store the output of the battery status command
 battery_info=$(pmset -g batt)
+# Extract the percentage using grep (looks for numbers followed by %)
+percentage=$(echo "$battery_info" | grep -o '[0-9]\+%' | head -1)
 
 # Check if the output contains 'AC Power'
 if echo "$battery_info" | grep -q "'AC Power'"; then
     # Print a Nerd Font lightning bolt icon (fa-bolt)
-    echo "󰚥"
+    if [[ -z "${percentage}" ]]; then
+        echo "100% 󰚥"
+    else
+        echo "$percentage 󰚥"
+    fi
 else
-    # Extract the percentage using grep (looks for numbers followed by %)
-    percentage=$(echo "$battery_info" | grep -o '[0-9]\+%' | head -1)
-    
+
     # Print the percentage alongside a Nerd Font heart icon (fa-heart)
     echo "$percentage "
 fi
